921dfd5e25bcaa74dd9a11d8d12ee2386fe94ec6
MissAnichka <annamedyukh@gmail.com>
2019-04-29 04:00:35 -0400

    removed unnecessary imports

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,26 +3,6 @@ import { Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 import * as actions from "../actions";
 import { connect } from 'react-redux';
 import "./assets/css/style.css";
-import {
-    Button,
-    ButtonGroup,
-    Card,
-    CardHeader,
-    CardBody,
-    CardTitle,
-    Container,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    UncontrolledDropdown,
-    Label,
-    FormGroup,
-    Input,
-    Table,
-    Row,
-    Col,
-    UncontrolledTooltip
-} from "reactstrap";
 
 class Home extends Component {
 
