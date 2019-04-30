28e85f158e6574664fbb8ceec606a54173be1641
joshua <joshuaaguilar20@gmail.com>
2019-04-30 03:18:05 -0700

    Updates

---
+++ precache-manifest.2c93a77addd506594a7ded5e1dfda7a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa13176ffa2cc50efc3168d72000d3e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a6b31d531f7762a75c4",
+    "url": "/static/css/2.bcdce821.chunk.css"
+  },
+  {
+    "revision": "3fa64ab4d5146d27a50f",
+    "url": "/static/css/main.9104c073.chunk.css"
+  },
+  {
+    "revision": "0a6b31d531f7762a75c4",
+    "url": "/static/js/2.eb541b2b.chunk.js"
+  },
+  {
+    "revision": "3fa64ab4d5146d27a50f",
+    "url": "/static/js/main.da1a0ee1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "841ef830fae3a7b3ab1fb020532b2bd1",
+    "url": "/static/media/child3.2019-04-29 15_47_54.841ef830.gif"
+  },
+  {
+    "revision": "619647462a9f89b933ae538486c84ac9",
+    "url": "/static/media/childgif.61964746.gif"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/static/media/react-logo.eb6be414.png"
+  }
+]);
